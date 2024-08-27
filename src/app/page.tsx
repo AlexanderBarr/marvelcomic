eec6f38f6f7b88e5e3c96be4c839fa3f3c57e970
Alex Barr <luke.daniels@approach-solutions.com>
2024-08-27 11:49:56 +1000

    feat-installed shadcn fixing modals

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,10 @@
-import Image from "next/image";
-import Link from "next/link";
 import MarvelCharacters from "./_components/MarvelCharacters";
-// import { getMyImages } from "~/server/queries";
-
 export const dynamic = "force-dynamic";
 
 export default async function HomePage() {
   return (
     <main className="">
       <MarvelCharacters />
-      {/* <SignedOut>
-        <div className="h-full w-full text-center text-2xl">
-          Please sign in above
-        </div>
-      </SignedOut>
-      <SignedIn>
-        <Images />
-      </SignedIn> */}
     </main>
   );
 }

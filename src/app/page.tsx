fa912d77dd4bb6be4e97a31ff00448982eb923b5
Alex Barr <luke.daniels@approach-solutions.com>
2024-08-27 11:10:49 +1000

    feat-fixing modal loading

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,22 @@
+import Image from "next/image";
 import Link from "next/link";
-import MarvelCharacters from "./_component/MarvelCharacters";
+import MarvelCharacters from "./_components/MarvelCharacters";
+// import { getMyImages } from "~/server/queries";
 
-export default function HomePage() {
+export const dynamic = "force-dynamic";
+
+export default async function HomePage() {
   return (
-    <main className="flex flex-wrap">
-      <MarvelCharacters /> {/* Use the client component */}
+    <main className="">
+      <MarvelCharacters />
+      {/* <SignedOut>
+        <div className="h-full w-full text-center text-2xl">
+          Please sign in above
+        </div>
+      </SignedOut>
+      <SignedIn>
+        <Images />
+      </SignedIn> */}
     </main>
   );
 }
